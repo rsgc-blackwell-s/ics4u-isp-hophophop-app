01ab58e0ebca69daf70a4226f8d5a97534b6ffa5
Scott Blackwell <sblackwell@rsgc.on.ca>
2017-02-22 20:00:14 -0500

    Fixed placement issue for now, still having trouble understanding where the origin is and how to place an image or label in respect to that origin...

--- Hop Hop Hop/Hop Hop Hop/GameScene.swift
+++ Hop Hop Hop/Hop Hop Hop/GameScene.swift
@@ -13,17 +13,17 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        let scoreLabelNode = SKLabelNode(fontNamed: "Marker Felt")
+        let scoreLabelNode = SKLabelNode(fontNamed: "Futura")
         scoreLabelNode.text = "SCORE"
         scoreLabelNode.fontSize = 40
         scoreLabelNode.fontColor = SKColor.white
-        scoreLabelNode.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height*0.75)
+        scoreLabelNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*0.4)
         self.addChild(scoreLabelNode)
         
         let karateKidNode = SKSpriteNode(imageNamed: "karateKid")
         karateKidNode.size = CGSize(width: 180, height: 180)
         karateKidNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        karateKidNode.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height*0.25)
+        karateKidNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*(-0.4))
         karateKidNode.zPosition = 100
         karateKidNode.name = "karateKid"
         self.addChild(karateKidNode)
