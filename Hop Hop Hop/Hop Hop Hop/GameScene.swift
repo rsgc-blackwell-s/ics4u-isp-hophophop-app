b5baf8a2c17929bd78856382d6758171f1809ecd
Scott Blackwell <sblackwell@rsgc.on.ca>
2017-02-23 14:43:59 -0500

    Made some changes including the background, and the character type... this is part of task #3

--- Hop Hop Hop/Hop Hop Hop/GameScene.swift
+++ Hop Hop Hop/Hop Hop Hop/GameScene.swift
@@ -13,9 +13,14 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        let background = SKSpriteNode(imageNamed: "fortuneCookieBackground")
+        background.position = CGPoint(x: self.size.height*0, y: self.frame.width*0)
+        background.size = CGSize(width: self.frame.size.width*2.3, height: self.frame.size.height)
+        self.addChild(background)
+        
         let scoreLabelNode = SKLabelNode(fontNamed: "Futura")
         scoreLabelNode.text = "SCORE"
-        scoreLabelNode.fontSize = 40
+        scoreLabelNode.fontSize = 60
         scoreLabelNode.fontColor = SKColor.white
         scoreLabelNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*0.4)
         self.addChild(scoreLabelNode)
@@ -23,7 +28,7 @@ class GameScene: SKScene {
         let karateKidNode = SKSpriteNode(imageNamed: "karateKid")
         karateKidNode.size = CGSize(width: 180, height: 180)
         karateKidNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        karateKidNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*(-0.4))
+        karateKidNode.position = CGPoint(x: self.frame.size.width*(-0.4), y: self.frame.size.height*(-0.4))
         karateKidNode.zPosition = 100
         karateKidNode.name = "karateKid"
         self.addChild(karateKidNode)
