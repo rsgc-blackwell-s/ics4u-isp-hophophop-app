4f50fc80c2c02d6f3edd3b0c7bd4890b28bd4192
Scott Blackwell <sblackwell@rsgc.on.ca>
2017-04-03 14:48:23 -0400

    score now updates after every touch... Part of issue #9

--- Hop Hop Hop/Hop Hop Hop/GameScene.swift
+++ Hop Hop Hop/Hop Hop Hop/GameScene.swift
@@ -17,6 +17,11 @@ class GameScene: SKScene {
     var actionJumpDown = SKAction()
     var actionJumpSequence = SKAction()
     
+    var scoreNode = SKLabelNode()
+    var score = 0
+    
+    
+    
     //Initial functions
     override func didMove(to view: SKView) {
         
@@ -32,15 +37,26 @@ class GameScene: SKScene {
         let background = SKSpriteNode(imageNamed: "fortuneCookieBackground")
         background.position = CGPoint(x: self.size.height*0, y: self.frame.width*0)
         background.size = CGSize(width: self.frame.size.width*2.3, height: self.frame.size.height)
+        background.zPosition = -1
         self.addChild(background)
         
-        //Making score label
+        // Making score label
         let scoreLabelNode = SKLabelNode(fontNamed: "Futura")
         scoreLabelNode.text = "SCORE"
         scoreLabelNode.fontSize = 60
         scoreLabelNode.fontColor = SKColor.white
         scoreLabelNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*0.4)
+        scoreLabelNode.zPosition = 1
         self.addChild(scoreLabelNode)
+        
+        // Show score under label
+        scoreNode = SKLabelNode(fontNamed: "Futura")
+        scoreNode.text = String(score)
+        scoreNode.fontSize = 40
+        scoreNode.fontColor = SKColor.gray
+        scoreNode.position = CGPoint(x: self.frame.size.width*0, y: self.frame.size.height*0.35)
+        scoreLabelNode.zPosition = 1
+        self.addChild(scoreNode)
         
         // Making game character
         karateKidNode = SKSpriteNode(imageNamed: "karateKid")
@@ -52,32 +68,40 @@ class GameScene: SKScene {
         self.addChild(karateKidNode)
         //        karateKidNode.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(10))
         karateKidNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 150, height: 150))
+    }
+    
+    func updateScore(){
         
+        scoreNode.text = String(score)
         
     }
     
     // When screen is touched...
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
         guard let touch = touches.first else {
-            
             return
-            
         }
         
+        // Count touches
+        score = score + 1
+        updateScore()
+        
+        // Track touch location
         let touchLocation = touch.location(in: self)
         
+        // Create value for distance from character to touch
         let dX = touchLocation.x - karateKidNode.position.x
         let dY = touchLocation.y - karateKidNode.position.y
         
-        //karateKidNode.run(actionJumpSequence)
+        // Apply an impulse in direction towards touch
         if let body = karateKidNode.physicsBody {
             body.applyImpulse(CGVector(dx: (dX), dy: (dY)*1.9))
         }
-        
     }
     
+    // Testing to make sure touch is registered (noticed)
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("test")
+        print(score)
     }
 }
